76cf58996e8dda8b35596a39dde514b3d39401b5
Esis Denis <presetlr@gmail.com>
2019-10-11 19:50:07 +0300

    init: start project

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,18 +1,38 @@
 class TicTacToe {
     constructor() {
-
+        this.field = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        this.winCombinations = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ];
+        this.stepCount = 0;
+        this.Player = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.Player ? 'o' : 'x';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.field[rowIndex][columnIndex] === null) {
+            this.field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+            this.Player = this.Player ? 0 : 1;
+            this.stepCount++;
+        }
     }
 
     isFinished() {
-
+        return this.stepCount === 9 || this.getWinner() !== null;
     }
 
     getWinner() {
@@ -28,7 +48,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
